b5b5da38453b63e260e24eb60e4a72dd10f9e68b
Jagadeesh1431 <jj8418064@gmail.com>
2024-08-27 21:22:54 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,13 +4,13 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = 7523039760:AAGNidnciWsMhYHMfeEN5Tq5ZVmvhNvJp5A
+ADMIN_ID = 6272351021
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-bot = Bot(API_TOKEN)
+bot = -4555738435
 
 def start_bot():
     """Start the bot script as a subprocess."""
